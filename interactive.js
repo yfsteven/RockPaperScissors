4e169ef2b8398b35e15e1e8fc8d4598a4d6d229b
yfsteven <yaninquiry@gmail.com>
2024-01-27 14:19:55 -0500

    Fix playRound() after many attempts to get the right setup

--- interactive.js
+++ interactive.js
@@ -1,17 +1,35 @@
-const threeValues = ['Paper', 'Rock', 'Scissors'];
-
-const playRound = (playerSelection, computerSelection) => {
-    const sanitizedPlayerChoice = threeValues.forEach((element) => { 
-        if(element.toLowerCase() === playerSelection.toLowerCase()) {
-            return element
-        } else {
-            return ""
-    }})
-}
+const threeValues = ['paper', 'rock', 'scissors'];
 
 let playerChoice = prompt("Pick Rock, Paper, or Scissors");
-const computerChoice = getComputerChoice();
+
+const playRound = (playerSelection, computerSelection) => {
+    const sanitizedPlayerSelection = playerSelection.toLowerCase();  
+    if (sanitizedPlayerSelection === computerSelection) {
+        console.log("It's a Tie!");
+    } else if (sanitizedPlayerSelection === 'rock') {
+        switch(computerSelection) {
+            case 'paper': console.log('You lost!'); break;
+            case 'scissors': console.log('You won!'); break;
+        }
+    } else if (sanitizedPlayerSelection === 'paper') {
+        switch(computerSelection) {
+            case 'rock': console.log('You won!'); break;
+            case 'scissors': console.log('You lost!'); break;
+        }
+    } else if (sanitizedPlayerSelection === 'scissors') {
+        switch(computerSelection) {
+            case 'paper': console.log('You won!'); break;
+            case 'rock': console.log('You lost!'); break;
+        }
+    } else {
+        console.log('Please select Rock, Paper, or Scissors');
+    }
+}
+
+
 
 const getComputerChoice = () => {
-    return threeValues[Math.floor(Math.random() * 3)]
+    return threeValues[Math.floor(Math.random() * 3)];
 }
+const computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice)
