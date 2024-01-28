8e90a5c9916e64e532288d4a46121edfcea795cc
yfsteven <yaninquiry@gmail.com>
2024-01-27 21:41:34 -0500

    Added a log feature to record matches

--- interactive.js
+++ interactive.js
@@ -1,8 +1,52 @@
 const threeValues = ['paper', 'rock', 'scissors'];
 
-/*
-let playerChoice = prompt("Pick Rock, Paper, or Scissors");
-const playRound = (playerSelection, computerSelection) => {
+const threeButtons = document.querySelectorAll('.ui-button');
+const logs = document.querySelector('#rounds-log');
+
+
+threeButtons.forEach((button) => {
+
+    button.addEventListener('click', () => {
+      playRound(`${button.innerHTML}`);
+    });
+});
+const getComputerChoice = () => {
+    return threeValues[Math.floor(Math.random() * 3)];
+}
+let playerCount = 0;
+let enemyCount = 0;
+let totalCount = 0;
+
+
+const playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice();
+    const sanitizedPlayerSelection = playerSelection.toLowerCase();  
+    if (sanitizedPlayerSelection === computerSelection) {
+        logs.innerHTML += `<p>It's a TIE!</p>`;
+    } else if (sanitizedPlayerSelection === 'rock') {
+        switch(computerSelection) {
+            case 'paper': logs.innerHTML += `<p>You lost!</p>`; enemyCount++; break;
+            case 'scissors': logs.innerHTML += `<p>You won!</p>`; playerCount++; break;
+        }
+    } else if (sanitizedPlayerSelection === 'paper') {
+        switch(computerSelection) {
+            case 'rock': logs.innerHTML += `<p>You won!</p>`; playerCount++; break;
+            case 'scissors': logs.innerHTML += `<p>You lost!</p>`; enemyCount++; break;
+        }
+    } else if (sanitizedPlayerSelection === 'scissors') {
+        switch(computerSelection) {
+            case 'paper': logs.innerHTML += `<p>You won!</p>`; playerCount++; break;
+            case 'rock': logs.innerHTML += `<p>You lost!</p>`; enemyCount++; break;
+        }
+    } else {
+        logs.innerHTML += `<p>Please select Rock, Paper, or Scissors</p>`;
+    }
+    totalCount = playerCount + enemyCount;
+    console.log(totalCount);
+}
+
+/*const playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice();
     const sanitizedPlayerSelection = playerSelection.toLowerCase();  
     if (sanitizedPlayerSelection === computerSelection) {
         console.log("It's a Tie!");
@@ -24,32 +68,9 @@ const playRound = (playerSelection, computerSelection) => {
     } else {
         console.log('Please select Rock, Paper, or Scissors');
     }
+    totalCount = playerCount + enemyCount;
+    console.log(totalCount);
 }
 
-
-
-const getComputerChoice = () => {
-    return threeValues[Math.floor(Math.random() * 3)];
-}
-let playerCount = 0;
-let enemyCount = 0;
-let totalCount = 0;
-
-const game = () => {
-    playerCount = 0;
-    enemyCount = 0;
-    totalCount = 0;
-    while(totalCount < 5) {
-        let playerChoice = prompt("Pick Rock, Paper, or Scissors");
-        const computerChoice = getComputerChoice();
-        totalCount = playerCount + enemyCount;
-        playRound(playerChoice, computerChoice)
-    }
-    if (playerCount > enemyCount) {
-        console.log(`You won ${playerCount} out of 5 rounds! Victory is Yours!!!`)
-    } else {
-        console.log(`Defeat. Enemy beat you by ${enemyCount} wins`)
-    }
-}
-game()
 */
+
